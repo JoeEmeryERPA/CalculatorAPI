85e62fc8f9893ae519e6ae3ea7133705da3b41bb
JoeEmeryERPA <87312218+JoeEmeryERPA@users.noreply.github.com>
2024-11-07 14:40:23 +1100

    Deleted legacy controller folder & activities
    Deleted /Controllers/JWTAuthController.cs & /Controllers/WeatherForecastController.cs

--- obj/Debug/net8.0/CalculatorAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalculatorAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c92b73c7342e50358e08c9a250f353cb9a8827e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45d9217fb290232651822e859cd1de5feaed2350")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
