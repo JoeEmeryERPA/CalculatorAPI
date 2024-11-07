6acd705c86b264304dce6a9cb6423d66f6107bfa
JoeEmeryERPA <87312218+JoeEmeryERPA@users.noreply.github.com>
2024-11-07 22:03:34 +1100

    Git Push before web Xunit & MVC app factory testing API pipeline test
    push before adding dependencies on Xunit & MVC app factory

--- obj/Debug/net8.0/CalculatorAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalculatorAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45d9217fb290232651822e859cd1de5feaed2350")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e62fc8f9893ae519e6ae3ea7133705da3b41bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
